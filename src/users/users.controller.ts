54135a0c70e59a2d1edd45120453a82aaae4af59
ucv <ucv@ucv.com>
2025-04-12 11:26:10 -0500

    Implement JWT authentication in UsersController; validate user role for access to getAllUsers endpoint

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,14 +1,41 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Headers, UnauthorizedException, ForbiddenException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';  // Asegúrate de importar el JwtService
 import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-
-    constructor(private readonly userService: UsersService) {}
+    constructor(
+        private readonly userService: UsersService,
+        private readonly jwtService: JwtService
+    ) { }
 
     @Get('getAll')
-    getAllUsers() {
-        return this.userService.getAllUsers();
-    }
+    async getAllUsers(@Headers('Authorization') headers: string) {
+        if (!headers) {
+            throw new UnauthorizedException({
+                message: 'Token not found',
+            });
+        }
 
+        const token = headers.replace('Bearer ', '');
+        const decoded = this.jwtService.decode(token);
+
+        if (!decoded || !decoded['role']) {
+            throw new UnauthorizedException({
+                message: 'Invalid token or role not found in token',
+            });
+        }
+
+        const userRole = decoded['role'];
+        const requiredRole = 'ADMIN';
+
+        if (userRole !== requiredRole) {
+            throw new ForbiddenException({
+                message: 'You do not have permission to access this resource',
+            });
+        }
+
+        return this.userService.getAllUsers();
+
+    }
 }
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -2,8 +2,12 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma.service';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.register({})
+  ],
   providers: [UsersService, PrismaService],
   controllers: [UsersController]
 })
