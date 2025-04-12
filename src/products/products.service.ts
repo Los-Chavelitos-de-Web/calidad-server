802d5d7cf39a5c570a095572a27c2d86eff7322d
Letalandroid <carlossoncra@gmail.com>
2025-04-11 19:02:40 -0500

    Corregir la ruta de importación de PrismaService en los módulos de productos

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaPromise } from '@prisma/client';
-import { Product } from 'src/models/Product';
-import { PrismaService } from 'src/prisma.service';
+import { PrismaService } from '../prisma.service';
 
 @Injectable()
 export class ProductsService {
-
-    constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) {}
 
   getProducts() {
     return this.prisma.product.findMany();
