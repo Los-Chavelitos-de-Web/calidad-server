0e54cbb1adcdb0e1b6113c0df54b25d7d0140dd2
ucv <ucv@ucv.com>
2025-04-12 11:26:03 -0500

    Eliminar modelo HeaderDTO ya que no se utiliza en la aplicación

--- src/models/HeadersAuth.ts
+++
@@ -1,9 +0,0 @@
-import { Expose } from "class-transformer";
-import { IsDefined, IsString } from "class-validator";
-
-export class HeaderDTO {
-    @IsString()
-    @IsDefined()
-    @Expose({ name: 'myheader1' })        // required as headers are case insensitive
-    token: string;
-}
