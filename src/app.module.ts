6b32af05458c97cdf63378b9f0a3ad4aa0e846ea
ucv <ucv@ucv.com>
2025-04-12 09:52:44 -0500

    Agregar módulo de autenticación y configuración global en AppModule; implementar validación global en main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [ProductsModule, UsersModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }), ProductsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
