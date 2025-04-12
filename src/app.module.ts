a9e6380c2507e2c1f2a6e5707aeb266b86a90502
ucv <ucv@ucv.com>
2025-04-12 10:37:12 -0500

    Agregar módulo de usuarios al AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,12 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
-  }), ProductsModule, AuthModule],
+  }), ProductsModule, AuthModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
