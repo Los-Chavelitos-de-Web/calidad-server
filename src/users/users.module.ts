c2a13d79c9c8bcd16a179581f68fdb730c8e7020
ucv <ucv@ucv.com>
2025-04-12 11:33:17 -0500

    Refactor JWT module configuration to use async registration with ConfigService for dynamic secret and expiration settings

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,19 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { PrismaService } from 'src/prisma.service';
 import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    JwtModule.register({})
+    ConfigModule.forRoot(),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        expiresIn: configService.get('JWT_EXPIRES_IN') || '60s'
+    }),
+    }),
   ],
   providers: [UsersService, PrismaService],
   controllers: [UsersController]
