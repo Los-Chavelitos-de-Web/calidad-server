9a0e53a5deb69e3918d4d788ec5151eea1f30e5e
Letalandroid <carlossoncra@gmail.com>
2025-04-13 10:14:50 -0500

    Refactor AuthService for improved readability; format code and enhance error handling in register and login methods

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/models/User';
 import { PrismaService } from 'src/prisma.service';
@@ -7,54 +11,55 @@ import { JWTConfig } from 'src/utils/jwt';
 
 @Injectable()
 export class AuthService {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+    private readonly jwtConfig: JWTConfig,
+  ) {}
 
-    constructor(private readonly prisma: PrismaService,
-        private readonly jwtService: JwtService,
-        private readonly jwtConfig: JWTConfig) { }
-
-    async register(user: User) {
-        try {
-            const r = await this.prisma.user.create({ data: user });
-            return {
-                message: 'User created successfully',
-                data: r
-            };
-        } catch (error) {
-            throw new NotFoundException({
-                message: 'Error creating user',
-                error: error.meta,
-            });
-        }
+  async register(user: User) {
+    try {
+      const r = await this.prisma.user.create({ data: user });
+      return {
+        message: 'User created successfully',
+        data: r,
+      };
+    } catch (error) {
+      throw new NotFoundException({
+        message: 'Error creating user',
+        error: error.meta,
+      });
     }
+  }
 
-    async login(user: User) {
-        const u = await this.prisma.user.findUnique({
-            where: {
-                email: user.email,
-            }
+  async login(user: User) {
+    const u = await this.prisma.user.findUnique({
+      where: {
+        email: user.email,
+      },
+    });
+
+    if (u) {
+      const authPassw = await comparePassword(user.password, u?.password);
+
+      if (authPassw) {
+        const payload = { userId: u.id, username: user.name, role: u.role };
+
+        return {
+          token: await this.jwtService.signAsync(
+            payload,
+            this.jwtConfig.getConfig(),
+          ),
+        };
+      } else {
+        throw new UnauthorizedException({
+          message: 'Password incorrect',
         });
-
-        if (u) {
-            const authPassw = await comparePassword(user.password, u?.password);
-
-            if (authPassw) {
-                
-                const payload = { userId: u.id, username: user.name, role: u.role };
-                
-                return {
-                    token: await this.jwtService.signAsync(payload, this.jwtConfig.getConfig()),
-                };
-
-            } else {
-                throw new UnauthorizedException({
-                    message: 'Password incorrect',
-                });
-            }
-        } else {
-            throw new UnauthorizedException({
-                message: 'User not exists',
-            });
-        }
+      }
+    } else {
+      throw new UnauthorizedException({
+        message: 'User not exists',
+      });
     }
-
+  }
 }
