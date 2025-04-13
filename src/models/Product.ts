6496ae1d53479a58b7601eb7f51b0800e83d4dda
Letalandroid <carlossoncra@gmail.com>
2025-04-13 11:41:01 -0500

    Agregar clase ProductPay con validaciones y cambiar 'name' a 'title' en la interfaz Product

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,8 +1,29 @@
+import { IsNotEmpty, IsNumber, IsString, MinLength } from "class-validator";
+
 export interface Product {
-    name: string,
-    description?: string,
-    brand?: string,
-    price?: number,
-    stock?: number,
-    createdAt?: Date
+  name: string;
+  description?: string;
+  brand?: string;
+  price?: number;
+  stock?: number;
+  createdAt?: Date;
+}
+
+export class ProductPay {
+
+  @IsNotEmpty()
+  id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(5)
+  title: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  quantity: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  unit_price: number;
 }
