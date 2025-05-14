72261cbe2e798f564242ce41e93ac1dcc4703bf1
Letalandroid <carlossoncra@gmail.com>
2025-05-13 19:28:49 -0500

    feat: import types and export function

--- src/utils/genFacturas.ts
+++ src/utils/genFacturas.ts
@@ -1,21 +1,16 @@
-const factura = async () => {
+import { ClienteType, ItemsType } from "src/types/facturas";
+
+export const factura = async (cliente: ClienteType, items: ItemsType[]) => {
   const res = await fetch('https://facturaciondirecta.com/API_SUNAT/post.php', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      ruc: '20512345678',
-      tipo: '03',
-      serie: 'F001',
-      correlativo: '00000001',
-      fecha_emision: '2023-10-01',
-      cliente_tipo_doc: '1',
-      cliente_num_doc: '12345678',
-      cliente_nombre: 'Juan Perez',
-      total_gravado: 100,
-      total_igv: 18,
-      total_a_pagar: 118,
+      cliente,
+      items,
+      // venta,
+      // empresa
     }),
   }
   );
