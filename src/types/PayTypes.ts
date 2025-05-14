d388097614033d773a8f3cd60471dd3012bf3e24
Letalandroid <carlossoncra@gmail.com>
2025-05-14 16:37:23 -0500

    feat: Refactor types to class

--- src/types/PayTypes.ts
+++ src/types/PayTypes.ts
@@ -1,20 +1,58 @@
+import { IsNotEmpty, IsString } from "class-validator";
 import { ProductPay } from "../../src/models/Product";
 
-export type PayData = {
-  items: ProductPay[];
-  correo: string;
+export class PayData {
+  @IsNotEmpty()
+  items: ProductPay[] = new Array<ProductPay>();
+
+  @IsString()
+  @IsNotEmpty()
+  correo: string = '';
 }
 
-export type PaySuccess = {
-  collection_id: string;
-  collection_status: string;
-  payment_id: string;
-  status: string;
-  external_reference: string | null;
-  payment_type: string;
-  merchant_order_id: string;
-  preference_id: string;
-  site_id: string;
-  processing_mode: string;
-  merchant_account_id: string | null;
+export class PaySuccess {
+  @IsString()
+  @IsNotEmpty()
+  collection_id: string = '';
+
+  @IsString()
+  @IsNotEmpty()
+  collection_status: string = '';
+
+  @IsString()
+  @IsNotEmpty()
+  payment_id: string = '';
+
+  @IsString()
+  @IsNotEmpty()
+  status: string = '';
+
+  @IsString()
+  @IsNotEmpty()
+  external_reference: string | null = '';
+
+  @IsString()
+  @IsNotEmpty()
+  payment_type: string = '';
+
+  @IsString()
+  @IsNotEmpty()
+  merchant_order_id: string = '';
+
+  @IsString()
+  @IsNotEmpty()
+  preference_id: string = '';
+
+  @IsString()
+  @IsNotEmpty()
+  site_id: string = '';
+
+  @IsString()
+  @IsNotEmpty()
+  processing_mode: string = '';
+
+  @IsString()
+  @IsNotEmpty()
+  merchant_account_id: string | null = '';
 }
+
