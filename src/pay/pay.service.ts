b078bc2880205dc47a5a144bbfac908c611e0e41
Letalandroid <carlossoncra@gmail.com>
2025-05-14 10:44:16 -0500

    feat: Save data sale to database

--- src/pay/pay.service.ts
+++ src/pay/pay.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { MercadoPagoConfig, Preference } from 'mercadopago';
-import { ProductPay } from 'src/models/Product';
+import { ProductPay } from '../../src/models/Product';
+import { PrismaService } from '../../src/prisma.service';
 
 const client = new MercadoPagoConfig({
   accessToken: process.env.MERCADOPAGO_API_KEY || '',
@@ -8,8 +9,18 @@ const client = new MercadoPagoConfig({
 
 @Injectable()
 export class PayService {
+
+  constructor(private readonly prisma: PrismaService) { }
+
   async createPreference(correo: string, products: ProductPay[]) {
     const pref = new Preference(client);
+
+    if (!correo) {
+      throw new NotFoundException({
+        message: 'El correo es requerido',
+        error: 'El correo es requerido',
+      });
+    }
 
     try {
       const res = await pref.create({
@@ -27,7 +38,39 @@ export class PayService {
       });
 
       const prefId = res.id;
-      console.log(res);
+
+      const userId = await this.prisma.user.findFirst({
+        where: {
+          email: correo,
+        },
+        select: {
+          id: true,
+        },
+      });
+
+      const items = products.map((item) => ({
+        saleId: prefId || '',
+        producto: item.title,
+        cantidad: item.quantity + '',
+        precio_base: item.unit_price + '',
+        codigo_sunat: '-',
+        codigo_unidad: 'ZZ',
+        tipo_igv_codigo: '10'
+      }));
+
+      await this.prisma.sale.create({
+        data: {
+          id: prefId || '',
+          userId: userId?.id || 0,
+          status: 'pending',
+          total: products.reduce((acc, item) => acc + item.unit_price * item.quantity, 0),
+        }
+      });
+
+      await this.prisma.saleItem.createMany({
+        data: items,
+      });
+
       return res;
     } catch (e) {
       console.error(`Error al crear la preferencia: ${e}`);
