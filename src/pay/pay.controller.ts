7f537eef77a4d8036c6199e7bdf0d919cfcfb422
Letalandroid <carlossoncra@gmail.com>
2025-05-14 10:43:33 -0500

    fix: Add Types to pay

--- src/pay/pay.controller.ts
+++ src/pay/pay.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { PayService } from './pay.service';
 import { ProductPay } from 'src/models/Product';
+import { PayData } from 'src/types/PayTypes';
 
 @Controller('pay')
 export class PayController {
-  constructor(private readonly payService: PayService) {}
+  constructor(private readonly payService: PayService) { }
 
   @Post()
-  payment(@Body() correo: string, @Body() prods: ProductPay[]) {
-    return this.payService.createPreference(correo, prods);
+  payment(@Body() payData: PayData) {
+    return this.payService.createPreference(payData.correo, payData.items);
   }
 }
