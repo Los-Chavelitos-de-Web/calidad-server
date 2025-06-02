1f3dd4d35bb9c6abcbc42b859a266271d27f6624
Letalandroid <carlossoncra@gmail.com>
2025-06-02 09:48:14 -0500

    feat: agregar configuración de JWT en los módulos Describe y Pay

--- src/describe/describe.module.ts
+++ src/describe/describe.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { DescribeController } from './describe.controller';
 import { DescribeService } from './describe.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot(),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        expiresIn: configService.get('JWT_EXPIRES_IN') || '60s',
+      }),
+    }),
+  ],
   controllers: [DescribeController],
-  providers: [DescribeService]
+  providers: [DescribeService],
 })
 export class DescribeModule {}
