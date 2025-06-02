98caf8f36e15d3904e3025793944a103140adf1a
Letalandroid <carlossoncra@gmail.com>
2025-06-02 15:03:19 -0500

    feat: actualizar controlador de reservas para incluir un nuevo endpoint y mejorar la gestión de errores

--- src/reservas/reservas.controller.ts
+++ src/reservas/reservas.controller.ts
@@ -1,23 +1,23 @@
-import { Controller, Get, NotFoundException } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
 import { ReservasService } from './reservas.service';
 
 @Controller('reservas')
 export class ReservasController {
   constructor(private readonly reservasService: ReservasService) {}
 
-  @Get()
+  @Get('getAll')
   getAll() {
     return this.reservasService.getAll();
   }
 
   @Get(':id')
-  async getOne(id: number) {
-    const data = await this.reservasService.getOne(id);
+  async getOne(@Param('id') id: string) {
+    const data = await this.reservasService.getOne(parseInt(id));
 
     if (!data) {
       throw new NotFoundException({
-        message: 'Sale not found',
-        error: 'No sale found with the provided ID',
+        message: 'Reserva not found',
+        error: 'No reserva found with the provided ID',
       });
     }
     return data;
