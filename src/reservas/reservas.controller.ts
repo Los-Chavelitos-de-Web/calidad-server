2eff4871f052f2b7684ce257c691eee68f94221e
Letalandroid <carlossoncra@gmail.com>
2025-06-02 15:07:48 -0500

    feat: agregar autenticación y documentación a los endpoints de reservas y ventas

--- src/reservas/reservas.controller.ts
+++ src/reservas/reservas.controller.ts
@@ -1,16 +1,46 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, UseGuards } from '@nestjs/common';
 import { ReservasService } from './reservas.service';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
+import { ApiBearerAuth, ApiHeader, ApiOperation, ApiResponse } from 'node_modules/@nestjs/swagger/dist';
 
 @Controller('reservas')
 export class ReservasController {
   constructor(private readonly reservasService: ReservasService) {}
 
   @Get('getAll')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Token JWT en formato Bearer',
+    required: true,
+  })
+  @ApiOperation({ summary: 'Devuelve la lista de todas las ventas' }) // Descripción breve del endpoint
+  @ApiResponse({
+    status: 201,
+    description: 'Devuelve la lista de todas las ventas',
+  }) // Respuesta esperada
   getAll() {
     return this.reservasService.getAll();
   }
 
   @Get(':id')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Token JWT en formato Bearer',
+    required: true,
+  })
+  @ApiOperation({ summary: 'Obtener una reserva individual' }) // Descripción breve del endpoint
+  @ApiResponse({
+    status: 201,
+    description: 'Obtener una reserva individual',
+  }) // Respuesta esperada
+  @ApiResponse({
+    status: 400,
+    description: 'Datos inválidos para obtener la reserva.',
+  }) // Respuesta en caso de error
   async getOne(@Param('id') id: string) {
     const data = await this.reservasService.getOne(parseInt(id));
 
--- src/sales/sales.controller.ts
+++ src/sales/sales.controller.ts
@@ -1,16 +1,46 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param, UseGuards } from '@nestjs/common';
 import { SalesService } from './sales.service';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
+import { ApiBearerAuth, ApiHeader, ApiOperation, ApiResponse } from 'node_modules/@nestjs/swagger/dist';
 
 @Controller('sales')
 export class SalesController {
   constructor(private readonly salesService: SalesService) {}
 
   @Get('getAll')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Token JWT en formato Bearer',
+    required: true,
+  })
+  @ApiOperation({ summary: 'Devuelve la lista de todas las ventas' }) // Descripción breve del endpoint
+  @ApiResponse({
+    status: 201,
+    description: 'Devuelve la lista de todas las ventas',
+  }) // Respuesta esperada
   async getAll() {
     return await this.salesService.getAll();
   }
 
   @Get('/:id')
+  @UseGuards(AuthGuard)
+  @ApiBearerAuth()
+  @ApiHeader({
+    name: 'Authorization',
+    description: 'Token JWT en formato Bearer',
+    required: true,
+  })
+  @ApiOperation({ summary: 'Devuelve una venta por individual' }) // Descripción breve del endpoint
+  @ApiResponse({
+    status: 201,
+    description: 'Devuelve una venta por individual',
+  }) // Respuesta esperada
+  @ApiResponse({
+    status: 400,
+    description: 'Datos inválidos para obtener la venta.',
+  }) // Respuesta en caso de error
   async getOne(@Param('id') id: string) {
     const data = await this.salesService.getOne(id);
 
