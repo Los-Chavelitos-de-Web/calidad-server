702135a70190e5acc78da7aa97db4f313be90140
Letalandroid <carlossoncra@gmail.com>
2025-06-02 09:48:19 -0500

    feat: proteger el endpoint de descripción con AuthGuard y agregar soporte para autenticación

--- src/describe/describe.controller.ts
+++ src/describe/describe.controller.ts
@@ -1,16 +1,29 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiResponse,
+  ApiBearerAuth,
+} from '@nestjs/swagger';
 import { ProductPromptType } from '../../src/types/promptTypes';
 import { describe } from '../../src/utils/getPrompt';
+import { AuthGuard } from '../guards/auth/auth.guard';
 
 @ApiTags('Describe') // Categoría para agrupar los endpoints relacionados con descripciones
+@UseGuards(AuthGuard)
+@ApiBearerAuth()
 @Controller('describe')
 export class DescribeController {
-
   @Post()
   @ApiOperation({ summary: 'Generar una descripción para un producto' }) // Descripción breve del endpoint
-  @ApiResponse({ status: 200, description: 'Descripción generada exitosamente.' }) // Respuesta esperada
-  @ApiResponse({ status: 400, description: 'Datos inválidos para generar la descripción.' }) // Respuesta en caso de error
+  @ApiResponse({
+    status: 200,
+    description: 'Descripción generada exitosamente.',
+  }) // Respuesta esperada
+  @ApiResponse({
+    status: 400,
+    description: 'Datos inválidos para generar la descripción.',
+  }) // Respuesta en caso de error
   descibre(@Body() p: ProductPromptType) {
     return describe(p);
   }
