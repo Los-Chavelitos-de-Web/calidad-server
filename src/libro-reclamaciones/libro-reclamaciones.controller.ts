aa1e1809f9fb06c42fb6023823be6a694516c3ec
Letalandroid <carlossoncra@gmail.com>
2025-07-08 22:04:20 -0500

    Add libro de reclamaciones

--- src/libro-reclamaciones/libro-reclamaciones.controller.ts
+++ src/libro-reclamaciones/libro-reclamaciones.controller.ts
@@ -1,4 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { LibroReclamacionesService } from './libro-reclamaciones.service';
+import { LibroReclamacionesCreate } from 'src/models/LibroReclamaciones';
 
 @Controller('libro-reclamaciones')
-export class LibroReclamacionesController {}
+export class LibroReclamacionesController {
+
+    constructor (private readonly libroService: LibroReclamacionesService) {}
+
+    @Get()
+    getAll() {
+        return this.libroService.getAll();
+    }
+
+    @Post()
+    create(@Body() libro: LibroReclamacionesCreate) {
+        return this.libroService.create(libro);
+    }
+
+    @Put(':id')
+    update(@Param('id') id, @Body() libro: LibroReclamacionesCreate) {
+        return this.libroService.update(parseInt(id), libro);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id) {
+        return this.libroService.delete(parseInt(id));
+    }
+
+}
--- src/libro-reclamaciones/libro-reclamaciones.service.ts
+++ src/libro-reclamaciones/libro-reclamaciones.service.ts
@@ -1,12 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { LibroReclamacionesCreate, LibroReclamacionesUpdate } from 'src/models/LibroReclamaciones';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class LibroReclamacionesService {
+  constructor(private readonly prisma: PrismaService) {}
 
-    constructor (private readonly prisma: PrismaService) {}
+  getAll() {
+    this.prisma.libroReclamaciones.findMany();
+  }
 
-    getAll() {
-    }
+  create(libroData: LibroReclamacionesCreate) {
+    return this.prisma.libroReclamaciones.create({
+      data: libroData,
+    });
+  }
 
+  update(id: number, libroData: LibroReclamacionesUpdate) {
+    return this.prisma.libroReclamaciones.update({
+      where: {
+        id,
+      },
+      data: libroData,
+    });
+  }
+
+  delete(id: number) {
+    return this.prisma.libroReclamaciones.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
